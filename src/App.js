2008a703956efa68c070b1a883892e92bf2f8d93
Arc_Anya <arcanyatwitch@gmail.com>
2021-11-25 10:13:31 +0100

    private route added for when login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import AuthProvider from './context/auth';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home'
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <Router>
         <Navbar />
         <Routes>
           <Route exact path='/register' element={<Register />} />
           <Route exact path='/login' element={<Login />} />
-          <Route exact path='/' element={<Home />} />
+          <Route exact path='/' element={<PrivateRoute />}>
+            <Route exact path='/' element={<Home />} />
+          </Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
     </AuthProvider>
   );
 }
