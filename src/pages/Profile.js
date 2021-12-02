1f8013b8c2a7be20fb70866eba6f0e3c188f32a6
Arc_Anya <arcanyatwitch@gmail.com>
2021-12-02 15:30:26 +0100

    correcting wrong import Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react/cjs/react.development'
+import React, { useState, useEffect } from 'react'
 import Camera from '../components/svg/Camera'
 import Delete from '../components/svg/Delete'
 import Img from '../icon-192.png'
